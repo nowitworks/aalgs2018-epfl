d19aedb0acad014d526862a297db5dd69e8dae16
rodamber <rodrigoambernardo@gmail.com>
2018-05-19 19:44:39 +0200

    Add homework 2 folder

---
+++ hw2/karger.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+  std::cout << "Hello world!" << std::endl;
+
+  int nedges, nvertices;
+
+  std::cin >> nvertices;
+  std::cin >> nedges;
+
+
+  std::cout << nvertices << nedges << std::endl;
+
+  return 0;
+}
