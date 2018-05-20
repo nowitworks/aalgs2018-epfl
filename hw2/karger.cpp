83e8d1198e47ac1b3b2ee9d97a4366b6cbc48e41
rodamber <rodrigoambernardo@gmail.com>
2018-05-20 16:06:41 +0200

    Add first try on the union-find data structure

--- hw2/karger.cpp
+++ hw2/karger.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
+#include <memory>
+
+
+template <typename T>
+class set {
+public:
+  T t;
+  std::shared_ptr<set> parent;
+  int rank;
+
+  // make-set
+  set(T &t): t(t), rank(0), parent(std::make_shared(this)) {
+  }
+
+  void link(set &that) {
+    if (this->rank > that->rank) {
+      that->parent = std::make_shared(this);
+    } else {
+      this->parent = std::make_shared(that);
+
+      if (this->rank == this->rank) {
+        that->rank = this->rank + 1;
+      }
+    }
+  }
+
+  // union
+  void set_union(set &that) {
+    find_set(this).link(find_set(that));
+  }
+
+  // find-set
+  set &find_set() const {
+    if (this != *parent) {
+      parent = std::make_shared(parent->find_set());
+    }
+    return *parent;
+  }
+
+};
 
 int main () {
   std::cout << "Hello world!" << std::endl;
