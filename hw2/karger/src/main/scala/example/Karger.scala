c13c9131af7356d53135b8dbfc292f94b7b32955
rodamber <rodrigoambernardo@gmail.com>
2018-05-26 00:01:45 +0200

    Edges are structured as Vectors
    We still have TLE on test 3

--- hw2/karger/src/main/scala/example/Karger.scala
+++ hw2/karger/src/main/scala/example/Karger.scala
@@ -5,7 +5,7 @@ object Karger extends App {
   val n = spec.n
   val times = n * n * math.ceil(math.log(n)).toInt // O(n^2 * log(n))
 
-  val cuts: HashSet[Set[(Int, Int)]] = HashSet()
+  val cuts: HashSet[Vector[(Int, Int)]] = HashSet()
 
   for (_ <- 1 to times) {
     val solver = new Solver(spec)
@@ -61,7 +61,7 @@ class Spec {
   def read = io.StdIn.readLine.split(" ").map(_.toInt)
 
   val Array(n, m) = read
-  val edges = for (_ <- 1 to m) yield {
+  val edges = Vector.tabulate(m) { _ =>
     val Array(u, v) = read
     (u, v)
   }
@@ -70,27 +70,22 @@ class Spec {
 class Solver(spec: Spec) {
   val vertices = for (i <- 1 to spec.n) yield new Node(i)
 
-  var edges = for ((u, v) <- spec.edges.toList) yield {
+  var edges = spec.edges.map { case (u, v) =>
     (vertices(u - 1), vertices(v - 1))
   }
 
-  def run(): Set[(Int, Int)] = {
+  def run(): Vector[(Int, Int)] = {
     for (_ <- 1 to spec.n - 2) {
       val (u, v) = pickEdge()
       u ++ v
       removeLoops()
     }
 
-    edges.map(x => (x._1.id, x._2.id)).toSet
+    edges.map(x => (x._1.id, x._2.id))
   }
 
   private def pickEdge(): (Node, Node) = {
-    val probabilities = List.tabulate(spec.m) { (x: Int) => 1.0 / (x + 1.0) }
-    val dummy = (new Node(0), new Node(0))
-
-    edges.zip(probabilities).foldLeft(dummy) {
-      case (z, (e, p)) => if (util.Random.nextDouble() <= p) e else z
-    }
+    edges(util.Random.nextInt(edges.size))
   }
 
   private def removeLoops() {
@@ -100,4 +95,3 @@ class Solver(spec: Spec) {
   }
 
 }
-
